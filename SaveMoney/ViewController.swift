4c4d3722b87ff7a08436f87041191094dc6b944b
渡邊丈洋 <mogu.224@gmail.com>
2016-07-24 15:01:29 +0900

    Initial Commit

---
+++ SaveMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveMoney
+//
+//  Created by 渡邊丈洋 on 2016/07/24.
+//  Copyright © 2016年 takehiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
